e770af0a3637a951e0065e7a0928a11c8f242726
Han from AWS EC2 <han@hanhuang.tech>
2022-04-21 15:16:47 +1200

    Add volume to web.sh, delete COPY from Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM nginx:stable-alpine
 COPY conf/nginx.conf /etc/nginx/nginx.conf
 COPY conf/hanhuang.tech.conf /etc/nginx/conf.d/
-COPY /html /usr/share/nginx/html
 EXPOSE 80
 EXPOSE 443
--- web.sh
+++ web.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 docker run \
 --name web \
+--rm \
 -it \
 -d \
 -v ${PWD}/../certbot_gen_docker/certs:/etc/letsencrypt \
+-v ${PWD}/html:/usr/share/nginx/html \
 -p 80:80 \
 -p 443:443 \
 web
