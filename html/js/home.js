aac3081fb6500d177373ad388101768321fd6afd
Han <han@hanhuang.tech>
2022-04-22 22:50:54 +1200

    Delete html directory

--- html/js/home.js
+++
@@ -1,6 +0,0 @@
-// animate habbit section
-const habbitToggle = document.querySelector("#habbit-toggle");
-
-habbitToggle.addEventListener("click", function () {
-    document.body.classList.toggle("habbit-move")
-});
