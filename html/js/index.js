c7738f2a95a4bcbe97139e75716f3aec02e182c7
hanhuang <han@hanhuang.tech>
2022-05-01 23:39:43 +1200

    Add new responsive in js

--- html/js/index.js
+++ html/js/index.js
@@ -1,6 +1,10 @@
 // animate logo
 const habbitToggle = document.querySelector("#habbit-toggle");
-
 habbitToggle.addEventListener("click", function () {
   document.body.classList.toggle("habbit-move");
 });
+
+const vpToggle = ["mbToggle","tbToggle","dtToggle","dthdToggle"]
+mbToggle = document.querySelector("#mb-toggle");
+vpToggle.addEventListener("click", function () {
+  document.body.classList.toggle("toggle-mobile")});
