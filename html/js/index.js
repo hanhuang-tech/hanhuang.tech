f6618607a1a671c8a3670cd86c990ed28bf2c122
hanhuang <han@hanhuang.tech>
2022-05-03 18:29:57 +1200

    Add changes to walk in funct on index.js

--- html/js/index.js
+++ html/js/index.js
@@ -1,11 +1,21 @@
+// toggle function
+function functToggle(t) {
+  document.body.classList.toggle(t);
+}
+
 // animate logo
 const habbitToggle = document.querySelector("#habbit-toggle");
-habbitToggle.addEventListener("click", function () {
-  document.body.classList.toggle("habbit-move");
-});
+habbitToggle.addEventListener("click", functToggle("habbit-move"));
 
+// walk in
 const walkin = document.querySelector("#stairs");
-
-walkin.addEventListener("mouseover", event => {
+walkin.addEventListener("mouseover", function () {
   document.body.classList.toggle("walk-in");
 });
+
+walkin.addEventListener("mouseleave", function () {
+  document.body.classList.toggle("walk-out");
+});
+
+
+
