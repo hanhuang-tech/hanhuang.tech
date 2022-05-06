2cc7859ed9081b1041e083585c26326332ce0ba5
hanhuang <han@hanhuang.tech>
2022-05-06 23:18:35 +1200

    Add new divider bridge, format han section

--- html/js/index.js
+++ html/js/index.js
@@ -11,11 +11,14 @@ habbitToggle.addEventListener("click", function () {
 
 // walk in
 const wipSect = document.querySelector("#wip");
-const walkImg = document.querySelector("#walkimg");
 const walkDiv = document.querySelector("#walkin");
+const walkImg = document.querySelector("#walkimg");
+const walkXh2 = document.querySelector("#walkh2");
 const revealHidden = document.querySelector("#hiddenwip");
+
 walkImg.addEventListener("mouseenter", function () {
   wipSect.style.background = "lightslategray";
+  walkXh2.style.color = "gray";
   walkDiv.style.opacity = 0;
   revealHidden.style.opacity = 1;
   revealHidden.style.zIndex = "1";
@@ -23,6 +26,7 @@ walkImg.addEventListener("mouseenter", function () {
 
 wipSect.addEventListener("mouseleave", function () {
   wipSect.style.background = "rgb(60, 60, 60)";
+  walkXh2.style.color = "white";
   walkDiv.style.opacity = 1;
   revealHidden.style.opacity = 0;
   revealHidden.style.zIndex = "0";
