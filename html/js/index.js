f93a63ecf0ab698befdd4569e51be85d95c17993
hanhuang <han@hanhuang.tech>
2022-05-08 02:10:23 +1200

    Add new resources styling and sections

--- html/js/index.js
+++ html/js/index.js
@@ -15,13 +15,16 @@ const walkDiv = document.querySelector("#walkin");
 const walkImg = document.querySelector("#walkimg");
 const walkXh2 = document.querySelector("#walkh2");
 const revealHidden = document.querySelector("#hiddenwip");
+const flowersBed2 = document.querySelector("#flowersbed2");
 
 walkImg.addEventListener("mouseenter", function () {
   wipSect.style.background = "lightslategray";
-  walkXh2.style.color = "gray";
+  walkXh2.style.color = "var(--clr-accent)";
   walkDiv.style.opacity = 0;
   revealHidden.style.opacity = 1;
   revealHidden.style.zIndex = "1";
+  flowersBed2.style.opacity = "0";
+  flowersBed2.style.zIndex = "0";
 });
 
 wipSect.addEventListener("mouseleave", function () {
@@ -30,4 +33,6 @@ wipSect.addEventListener("mouseleave", function () {
   walkDiv.style.opacity = 1;
   revealHidden.style.opacity = 0;
   revealHidden.style.zIndex = "0";
+  flowersBed2.style.opacity = "1";
+  flowersBed2.style.zIndex = "1";
 });
