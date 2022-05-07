834d6ebddaf805e3305e61ce1d69f336dd78513d
Han from AWS EC2 <han@hanhuang.tech>
2022-05-07 15:27:54 +1200

    Add new file reload.sh

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,3 @@
 FROM nginx:stable-alpine
-COPY conf/nginx.conf /etc/nginx/nginx.conf
-COPY conf/hanhuang.tech.conf /etc/nginx/conf.d/
 EXPOSE 80
 EXPOSE 443
--- web.sh
+++ web.sh
@@ -5,6 +5,8 @@ docker run \
 -d \
 -v ${PWD}/../certgen_docker/certs:/etc/letsencrypt \
 -v ${PWD}/html:/usr/share/nginx/html \
+-v ${PWD}/conf/nginx.conf:/etc/nginx/nginx.conf \
+-v ${PWD}/conf/hanhuang.tech.conf:/etc/nginx/conf.d/hanhuang.tech.conf \
 -p 80:80 \
 -p 443:443 \
 web
