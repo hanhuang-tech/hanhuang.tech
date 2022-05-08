3f8e6283c777f3498c4690ab6a4e58c48afd16ac
hanhuang <han@hanhuang.tech>
2022-05-08 22:51:37 +1200

    Update flowers2 for index.js

--- html/js/index.js
+++ html/js/index.js
@@ -15,7 +15,7 @@ const walkDiv = document.querySelector("#walkin");
 const walkImg = document.querySelector("#walkimg");
 const walkXh2 = document.querySelector("#walkh2");
 const revealHidden = document.querySelector("#hiddenwip");
-const flowersBed2 = document.querySelector("#flowersbed2");
+const flowers2 = document.querySelector("#flowers2");
 
 walkImg.addEventListener("mouseenter", function () {
   wipSect.style.background = "lightslategray";
@@ -23,8 +23,8 @@ walkImg.addEventListener("mouseenter", function () {
   walkDiv.style.opacity = 0;
   revealHidden.style.opacity = 1;
   revealHidden.style.zIndex = "1";
-  flowersBed2.style.opacity = "0";
-  flowersBed2.style.zIndex = "0";
+  flowers2.style.opacity = "0";
+  flowers2.style.zIndex = "0";
 });
 
 wipSect.addEventListener("mouseleave", function () {
@@ -33,6 +33,6 @@ wipSect.addEventListener("mouseleave", function () {
   walkDiv.style.opacity = 1;
   revealHidden.style.opacity = 0;
   revealHidden.style.zIndex = "0";
-  flowersBed2.style.opacity = "1";
-  flowersBed2.style.zIndex = "1";
+  flowers2.style.opacity = "1";
+  flowers2.style.zIndex = "1";
 });
