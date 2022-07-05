2b3b3c035520702df7bcdb9ff887274f21ce2d41
Han from AWS EC2 <han@hanhuang.tech>
2022-07-05 21:33:34 +1200

    Add new conf and TLS to hht

--- web.sh
+++ web.sh
@@ -3,10 +3,12 @@ docker run \
 --name web \
 -it \
 -d \
+-v ${PWD}/../certgen_docker:/letsencrypt \
 -v ${PWD}/../certgen_docker/certs:/etc/letsencrypt \
 -v ${PWD}/html:/usr/share/nginx/html \
 -v ${PWD}/conf/nginx.conf:/etc/nginx/nginx.conf \
 -v ${PWD}/conf/hanhuang.tech.conf:/etc/nginx/conf.d/hanhuang.tech.conf \
+-v ${PWD}/conf/clothingsite.conf:/etc/nginx/conf.d/clothingsite.conf \
 -p 80:80 \
 -p 443:443 \
 web
